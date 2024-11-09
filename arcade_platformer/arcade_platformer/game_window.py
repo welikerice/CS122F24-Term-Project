044a54692c28e046796483682c59bab9efb2e8db
Willy <tang3kaw@gmail.com>
2024-11-08 17:29:18 -0800

    Added Platform Map to Game Window
    Adjusted window dimensions, color, and added the platform map to the window

--- arcade_platformer/arcade_platformer/game_window.py
+++ arcade_platformer/arcade_platformer/game_window.py
@@ -4,25 +4,34 @@ import arcade
 
 # Game constants
 # Window dimensions
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
-SCREEN_TITLE = "Arcade Platformer"
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Deprecated King"
+
 
 # Assets path
 ASSETS_PATH = pathlib.Path(__file__).resolve().parent.parent / "assets"
+
+# Map scaling
+MAP_SCALING = 1.0
 
 
 class Platformer(arcade.Window):
     def __init__(self) -> None:
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.csscolor.SKY_BLUE)
 
         # These lists will hold different sets of sprites
         self.coins = None
         self.background = None
-        self.walls = None
+        self.collisions = None
         self.ladders = None
         self.goals = None
         self.enemies = None
+
+        # mainly for displaying the platform i guess
+        self.scene = None
+
 
         # One sprite for the player, no more is needed
         self.player = None
@@ -37,19 +46,33 @@ class Platformer(arcade.Window):
         self.level = 1
 
         # Load up our sounds here
-        self.coin_sound = arcade.load_sound(
-            str(ASSETS_PATH / "sounds" / "coin.wav")
-        )
-        self.jump_sound = arcade.load_sound(
-            str(ASSETS_PATH / "sounds" / "jump.wav")
-        )
-        self.victory_sound = arcade.load_sound(
-            str(ASSETS_PATH / "sounds" / "victory.wav")
-        )
+        # self.coin_sound = arcade.load_sound(
+        #     str(ASSETS_PATH / "sounds" / "coin.wav")
+        # )
+        # self.jump_sound = arcade.load_sound(
+        #     str(ASSETS_PATH / "sounds" / "jump.wav")
+        # )
+        # self.victory_sound = arcade.load_sound(
+        #     str(ASSETS_PATH / "sounds" / "victory.wav")
+        # )
 
     def setup(self):
         """Sets up the game for the current level"""
-        pass
+
+        map = ASSETS_PATH / "maps" / "platformer_map1.json"
+        layer_options = {
+            "Platforms Layer": {"use_spatial_hash": True,},
+        }
+        # tile_map = arcade.tilemap.TileMap(map, layer_options)
+        self.collisions = arcade.load_tilemap(map, 1, layer_options)
+        self.scene = arcade.Scene.from_tilemap(self.collisions)
+        # scene is needed, cannot directly use tilemap as scene
+
+
+        # self.physics_engine = arcade.PhysicsEnginePlatformer(None, platforms=self.collision_list)
+
+
+        # pass
 
     def on_key_press(self, key: int, modifiers: int):
         """Arguments:
@@ -72,7 +95,12 @@ class Platformer(arcade.Window):
         pass
 
     def on_draw(self):
-        pass
+        arcade.start_render()
+
+
+        self.scene.draw()
+
+        # pass
 
 
 if __name__ == "__main__":
